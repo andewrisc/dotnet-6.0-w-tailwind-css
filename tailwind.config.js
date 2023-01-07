2438a62c764e97404bac80efb0e8acaf2b9b3b41
zendyajah <zendy.za@outlook.com>
2023-01-08 01:12:15 +0700

    init commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+     './Pages/**/*.{cshtml,html,js}',
+     './Views/**/*.{cshtml,html,js}'
+],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
